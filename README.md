ae688939dc18adc18610e0bdc99c09df7eae0af6
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-21 13:50:02 +0000

    version 0.3.0

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ ropercenter
 
 The [Roper Center for Public Opinion Research](http://ropercenter.cornell.edu), in its own words, works "to collect, preserve, and disseminate public opinion data; to serve as a resource to help improve the practice of survey research; and to broaden the understanding of public opinion through the use of survey data in the United States and around the world."  It maintains the largest archive of public opinion data in existence, holding data dating back to the 1930s and from over 100 countries.  Researchers taking advantage of these datasets, however, are caught in a bind.  The terms and conditions for downloading any Roper Center dataset state that datasets "may not be resold or re-disseminated." But to ensure that one's work can be reproduced, assessed, and built upon by others, one must provide access to the raw data one employed.  
 
-The `ropercenter` package cuts this knot by providing programmatic, reproducible access to specified Roper Center datasets from within R for [registered users](https://ropercenter.cornell.edu/make-personalized-account/) at the Roper Center's [member institutions](https://ropercenter.cornell.edu/list-of-members/). 
+The `ropercenter` package cuts this knot by providing programmatic, reproducible access to specified Roper Center datasets from within R for [registered users](https://ropercenter.cornell.edu/make-personalized-account/) at the Roper Center's [member institutions](https://ropercenter.cornell.edu/membership/list-members). 
 
 
-To install:
+`ropercenter` requires an installation of the latest version of Chrome to run; [get Chrome here](https://www.google.com/chrome/).  Once you have installed Chrome, to install `ropercenter`:
 
 * the latest released version: `install.packages("ropercenter")`
 * the latest development version:
@@ -19,8 +19,8 @@ if (!require(remotes)) install.packages("remotes")
 remotes::install_github("fsolt/ropercenter")
 ```
 
-For more details, check out [the vignette](https://cran.r-project.org/package=ropercenter/vignettes/ropercenter-vignette.html).
+For more details, check out [the vignette](https://fsolt.org/ropercenter/articles/ropercenter-vignette.html).
 
-Please recall that by using Roper Center services, you accept the Center's [Terms and Conditions](https://ropercenter.cornell.edu/CFIDE/cf/action/registration/termsAndConditions.cfm).
+Please recall that by using Roper Center services, you accept the Center's [Terms and Conditions](https://ropercenter.cornell.edu/roper-center-data-archive-terms-and-conditions).
 
 
--- inst/doc/ropercenter-vignette.R
+++ inst/doc/ropercenter-vignette.R
@@ -1,32 +1,43 @@
-## ----eval = FALSE--------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  options("roper_email" = "juanita-herrera@uppermidwest.edu",
 #          "roper_password" = "password123!")
 
-## ----eval=FALSE----------------------------------------------------------
-#  roper_download(file_id = "USORCCNN2015-010")
+## ----eval=FALSE---------------------------------------------------------------
+#  roper_download(file_id = "31117412",
+#                 download_dir = tempdir()) # remember to specify a directory for your download
 
-## ----eval=FALSE----------------------------------------------------------
-#  roper_download(file_id = c("USORCCNN2015-010", "USORCCNN2015-009", "USORCCNN2015-008"))
+## ----eval=FALSE---------------------------------------------------------------
+#  roper_download(file_id = "USORCCNN2015-010",
+#                 download_dir = tempdir()) # remember to specify a directory for your download
 
-## ----eval=FALSE----------------------------------------------------------
-#  orccnn2015_010 <- rio::import("roper_data/USORCCNN2015-010/USORCCNN2015-010.RData")
+## ----eval=FALSE---------------------------------------------------------------
+#  roper_download(file_id = c("31117412", "USORCCNN2015-010"),
+#                 download_dir = tempdir()) # remember to specify a directory for your download
 
-## ----eval=FALSE----------------------------------------------------------
-#  roper_download("USAIPO1982-1197G")            # Gallup Poll for June 25-28, 1982 (ASCII only)
-#  gallup1982 <- read_ascii(file = "roper_data/USAIPO1982-1197G/USAIPO1982-1197G.dat",
+## ----eval=FALSE---------------------------------------------------------------
+#  orccnn2015_010 <- rio::import(file.path(tempdir(), "USORCCNN2015-010","USORCCNN2015-010.RData"))
+
+## ----eval=FALSE---------------------------------------------------------------
+#  roper_download("USAIPO1982-1197G", # Gallup Poll for June 25-28, 1982 (ASCII only)
+#                 download_dir = tempdir())  # remember to specify a directory for your download
+#  
+#  gallup1982 <- read_ascii(file = file.path(tempdir(), "USAIPO1982-1197G",
+#                                            "1197.dat"),
 #                           var_names = c("q09j", "weight"),
 #                           var_positions = c(38, 1),
 #                           var_widths = c(1, 1))
 
-## ----eval=FALSE----------------------------------------------------------
-#  library(ropercenter)
-#  roper_download("USAIPOCNUS1996-9603008")      # Gallup/CNN/USA Today Poll: Politics/1996 Election (ASCII only)
-#  gallup1996 <- read_ascii(file = "roper_data/USAIPOCNUS1996-9603008/USAIPOCNUS1996-9603008.dat",
+## ----eval=FALSE---------------------------------------------------------------
+#  roper_download("USAIPOCNUS1996-9603008", # Gallup/CNN/USA Today Poll: Politics/1996 Election (ASCII only)
+#                 download_dir = tempdir())  # remember to specify a directory for your download
+#  
+#  gallup1996 <- read_ascii(file = file.path(tempdir(), "USAIPOCNUS1996-9603008",
+#                                            "a9603008.dat"),
 #                           var_names = c("q43a", "q44", "weight"),
 #                           var_cards = c(6, 6, 1),
 #                           var_positions = c(62, 64, 13),
 #                           var_widths = c(1, 1, 3),
 #                           total_cards = 7,
-#                           card_pattern = "(?<=^.{10})\\d",
-#                           respondent_pattern = "(?<=^\\s{2})\\d{4}")
+#                           card_pattern = "(?<=^.{10})\\d", # a digit, preceded by the start of the line and ten other characters
+#                           respondent_pattern = "(?<=^\\s{2})\\d{4}") # four digits, preceded by the start of the line and two whitespace characters
 
